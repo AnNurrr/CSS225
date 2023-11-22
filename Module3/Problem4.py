f6dc86bf7f8c74396c9e0635cb9500577410770a
AnNurrr <ainuremilbekova9@gmail.com>
2023-11-22 14:41:10 -0600

    Module 3

---
+++ Module3/Problem4.py
@@ -0,0 +1,8 @@
+# Ainur
+# 10/15/2023
+# Computes the area of circle
+import math
+radius = float(input("Please enter a radius of your circle: "))
+area_of_circle = 3.14 * (radius ** 2)
+
+print("The area of your circle with radius " + str(radius) + " is " + str(area_of_circle))
---
+++ Module3/Problem6.py
@@ -0,0 +1,6 @@
+# Ainur
+# 10/15/2023
+# converts degrees Fahrenheit to degrees Celsius
+fahr = int(input("Enter temperature Fahrenheit: "))
+c = (fahr-32)*5/9
+print("The temperature in Celcius is " + str(int(c)))
