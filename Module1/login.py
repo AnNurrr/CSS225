adee61efb54cf1bb482a1bb2925492b05ef905ec
AnNurrr <ainuremilbekova9@gmail.com>
2023-11-23 16:27:24 -0600

    Module 1

---
+++ Module1/login.py
@@ -0,0 +1,15 @@
+#Ainur
+#10/2/2023
+#Checks the login unput
+
+
+#!/usr/bin/env python
+username = input("Login: >> ")
+user1 = "Jack"
+user2 = "Jill"
+if username == user1:
+    print("Access granted")
+elif username == user2:
+    print("Welcome to the system")
+else:
+    print("Access denied")
