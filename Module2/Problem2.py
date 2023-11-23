000982b1435735e828edfc515a68626b1f1a4772
AnNurrr <ainuremilbekova9@gmail.com>
2023-11-22 18:58:38 -0600

    Module 2

---
+++ Module2/Problem2.py
@@ -0,0 +1,10 @@
+# Ainur
+# 10/07/2023
+
+# pseudocode that asks a user for their name and greets them with their name
+
+# 1. Start
+# 2. Output: "Please enter your username: "
+# 3. Input user_name
+# 4. print("Hello World" + user_name)
+# 5. End
