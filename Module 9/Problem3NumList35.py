94c5d2b2b3c86a1ec934ec52bac7458443923b10
AnNurrr <ainuremilbekova9@gmail.com>
2023-11-28 16:02:36 -0600

    Module 9 Lab, new line of code

--- Module 9/Problem3NumList35.py
+++ Module 9/Problem3NumList35.py
@@ -2,13 +2,13 @@
 # 11/28/2023
 # asks the user a number and adds up each value until the sum is greater than 100
 
-user_value = int(input("Enter a number: "))
-print(user_value)
-List = 0
+
+
 sum = 0
 
 
 while sum<=100:
+    user_value = int(input("Enter a number: "))
+    sum+=user_value
 
-    sum += user_value
-    print(user_value)
+print("Sum is greater than 100, Final sum is: ", sum)
