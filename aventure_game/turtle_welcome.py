0965daa1d8272c4aa2971cc460187989d8cdff10
AnNurrr <ainuremilbekova9@gmail.com>
2023-12-08 18:12:53 -1200

    adventure game

---
+++ aventure_game/turtle_welcome.py
@@ -0,0 +1,11 @@
+import turtle
+
+def welcome():
+    turtle.bgcolor("black")
+    turtle.pencolor("White")
+    turtle.write("Welcome to Adventure Game!!!", align="center" , font=("Sans" , 30, "bold"))
+
+    turtle.exitonclick()
+
+    turtle.done()
+    turtle.mainloop()
